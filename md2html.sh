4a79425406bcdb9beac4e14684fcda79615cc44c
Peter Humburg <humburg@well.ox.ac.uk>
2014-11-21 22:08:49 +0000

    Added pandoc-templates submodule

--- md2html.sh
+++ md2html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc -t revealjs -s git-intro.md --standalone -o git-intro.html --css slides.css --toc --toc-depth=1 --template=../pandoc-templates/default.revealjs -V toc-header="Overview"
+pandoc -t revealjs -s git-intro.md --standalone -o git-intro.html --css slides.css --toc --toc-depth=1 --template=pandoc-templates/default.revealjs -V toc-header="Overview"
